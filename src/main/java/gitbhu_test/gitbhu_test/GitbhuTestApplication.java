ad7e0910a7b83ff4adba7e5ab37db8f43ae41a62
AlexinDPK <gim3187@gmail.com>
2023-08-08 13:01:28 +0900

    initial.1.0.3

---
+++ src/main/java/gitbhu_test/gitbhu_test/GitbhuTestApplication.java
@@ -0,0 +1,13 @@
+package gitbhu_test.gitbhu_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitbhuTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitbhuTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gitbhu_test/gitbhu_test/GitbhuTestApplicationTests.java
@@ -0,0 +1,13 @@
+package gitbhu_test.gitbhu_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitbhuTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
